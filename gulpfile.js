bd8048b569f032fb8246539b0a7db64e24e9b93b
Ning Ma <ningma1022@gmail.com>
2022-12-08 21:57:59 +1000

    partials

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
+// *.scss means we are getting/accessing/monitoring all scss files,
+// EXCEPT scss files that the files name starts with _
+
 function buildStyles() {
-  return src("index.scss").pipe(sass.sync()).pipe(dest("css"));
+  return src("*.scss").pipe(sass.sync()).pipe(dest("css"));
 }
 
 function watchTask() {
-  watch(["index.scss"], buildStyles);
+  watch(["*.scss"], buildStyles);
 }
 
 exports.buildStyles = buildStyles;
